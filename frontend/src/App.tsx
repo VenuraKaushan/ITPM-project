4b52eacd105aba0ae648b06421b8aa40f44de5b9
Venura Kaushan <86141130+VenuraKaushan@users.noreply.github.com>
2024-03-12 01:45:49 +0530

    create scroll in landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import "./App.css";
+import { MantineProvider, Container } from '@mantine/core';
 
 function App() {
   return (
-    <>
+    <MantineProvider>
+      <Container>
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
         </Routes>
       </Router>
-    </>
+      </Container>
+    </MantineProvider>
   );
 }
 
