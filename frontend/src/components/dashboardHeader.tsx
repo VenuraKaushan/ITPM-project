5fabc1e97eddceefdd5324e4176e6e0d2de4b32e
Vinnath Pathirana <125301413+VinnathPathirana@users.noreply.github.com>
2024-03-30 02:09:14 +0530

    commit to my branch

--- frontend/src/components/dashboardHeader.tsx
+++ frontend/src/components/dashboardHeader.tsx
@@ -26,7 +26,7 @@ import classes from '../Styles/HeaderTabs.module.css';
 import logo from "../assets/testlogo.png"
 
 const user = {
-    name: 'Jane Spoonfighter',
+    name: 'Vinnath ',
     email: 'janspoon@fighter.dev',
     image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png',
 };
